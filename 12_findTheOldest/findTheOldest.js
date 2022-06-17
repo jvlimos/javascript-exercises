aa3a95a2028f19bac4721ee703b9646635dc147b
jvlimos <johnvincentlimos@yahoo.com>
2022-06-17 19:05:41 +0800

    Do findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+	let yearsLived = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    
+    if (!arr[i].yearOfDeath) {
+      yearsLived.push(new Date().getFullYear() - arr[i].yearOfBirth);
+    } else {
+      yearsLived.push(arr[i].yearOfDeath - arr[i].yearOfBirth);
+    }
+    
+  }
+
+  return arr[yearsLived.indexOf(Math.max(...yearsLived))];
 
 };
 
