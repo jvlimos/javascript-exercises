bb496d8f2af93eca8f59d7d8d68d727084fdb251
jvlimos <johnvincentlimos@yahoo.com>
2022-06-17 15:39:10 +0800

    Do calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let total = 0;
+	for (let item of a) {
+		total += item;
+	}
+	return total;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+	let total = 1;
+	for (let item of a) {
+		total *= item;
+	}
+	return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+if (a == 0) return 1;
+else {
+	return a * (factorial(a - 1));
+}
 };
 
 // Do not edit below this line
