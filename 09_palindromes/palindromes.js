ca8086fb4ea28d72cd1213f2d3cf0704254daac8
jvlimos <johnvincentlimos@yahoo.com>
2022-06-17 16:17:27 +0800

    Do palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,30 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+	let lowerStr = str.toLowerCase();
+
+	function isAlphabet(character) {
+		if (character.charCodeAt() > 96 && character.charCodeAt() < 123) {
+			return true;
+		}
+	}
+    
+	let newStr = [];
+	for (let char of lowerStr) {
+		if (isAlphabet(char)) {
+			newStr.push(char);
+		}
+	}
+
+	let left = 0;
+	let right = newStr.length - 1;
+	let compareChars = [newStr[left], newStr[right]];
+	for (; left < Math.floor(newStr.length / 2); left++) {
+		compareChars = [newStr[left], newStr[right]];
+		if (compareChars[0] !== compareChars[1]) return false;
+		right--;
+	}
+
+	return true;
 
 };
 
